8a3e730d803274a53f69990ba61eae803ac3f875
alesurankar <ale.urankar@gmail.com>
2025-09-04 11:09:04 +0200

    connecting to database

--- server/main.py
+++ server/main.py
@@ -1,7 +1,18 @@
+from contextlib import asynccontextmanager
+from database import create_db_and_tables
+
 from fastapi import FastAPI
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load on start up
+    create_db_and_tables()
+    yield
+    print("Shutting down")
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
